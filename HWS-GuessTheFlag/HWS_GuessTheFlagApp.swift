82c4aa110cd9b55782036fdba3d4c837a322a4be
Alex Clark <alexander.d.clark@outlook.com>
2021-11-11 07:45:40 -0600

    Initial Commit

---
+++ HWS-GuessTheFlag/HWS_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_GuessTheFlagApp.swift
+//  HWS-GuessTheFlag
+//
+//  Created by Alexander Clark on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
